9d9491b0c26958c377ee80733f1e19654a8edd58
LE Ba Khanh Hoang <tennaythatdep12345@gmail.com>
2026-03-10 12:36:03 +0100

    Fix: Update vite.config.js to load .env variables at runtime for localhost development

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
-  }
+  plugins: [react()]
 })
